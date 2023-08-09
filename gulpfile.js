66419fa57b2de16badd60c2b972f18c8e380f584
kmn <storrymn@gmail.com>
2023-08-10 08:56:28 +0900

    git init

---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+/*
+npm i -g gulp
+npm i browser-sync gulp-beautify gulp-file-include gulp-autoprefixer gulp-plumber gulp-if delete
+*/
+
+const gulp = require("gulp");
+const sync = require("browser-sync").create();
+const beautify = require("gulp-beautify");
+const include = require("gulp-file-include");
+const prefix = require("gulp-autoprefixer");
+const plumber = require("gulp-plumber");
+const gulpIf = require("gulp-if");
+const del = require("delete");
+const paths = require("./config.js");
+const { series, parallel, watch } = gulp;
+
+function browserSync(done) {
+	sync.init({
+		port: 8080,
+		server: {
+			baseDir: "./",
+			index: "index.html",
+		},
+		browser: ["google chrome", "chrome"],
+		// browser: ['google chrome', 'chrome', 'firefox', 'iexplore', 'opera', 'safari']
+	});
+	done();
+}
+
+function reload(done) {
+	sync.reload();
+	done();
+}
+
+function init(done) {
+	del(paths.devAll, function (err, deleted) {
+		if (err) throw err;
+		console.log(deleted);
+	});
+	done();
+}
+
+function htmlBuild() {
+	return gulp
+		.src(paths.srcPath.html)
+		.pipe(plumber())
+		.pipe(
+			include({
+				prefix: "@@",
+				basepath: paths.srcPath.include,
+			})
+		)
+		.pipe(beautify.html({ indent_size: 2, indent_with_tabs: true }))
+		.pipe(gulp.dest(paths.devPath.html));
+}
+
+function cssBuild() {
+	return gulp
+		.src(paths.srcPath.css, { since: gulp.lastRun(cssBuild) })
+		.pipe(plumber())
+		.pipe(
+			prefix({
+				overrideBrowserslist: ["last 2 versions"],
+				cascade: false,
+			})
+		)
+		.pipe(beautify.css({ indent_size: 2, indent_with_tabs: true }))
+		.pipe(gulp.dest(paths.devPath.css));
+}
+
+function jsBuild() {
+	return gulp
+		.src(paths.srcPath.js, { since: gulp.lastRun(jsBuild) })
+		.pipe(plumber())
+		.pipe(
+			gulpIf(
+				(file) => !file.path.endsWith(".min.js"),
+				beautify({ indent_size: 2, indent_with_tabs: true })
+			)
+		)
+		.pipe(gulp.dest(paths.devPath.js));
+}
+
+function imageBuild() {
+	return gulp
+		.src(paths.srcPath.img, { since: gulp.lastRun(imageBuild) })
+		.pipe(plumber())
+		.pipe(gulp.dest(paths.devPath.img));
+}
+
+function watcher() {
+	watch(paths.srcPath.html, series(htmlBuild, reload));
+	watch(paths.srcPath.include, series(htmlBuild, reload));
+	watch(paths.srcPath.css, series(cssBuild, reload));
+	watch(paths.srcPath.js, series(jsBuild, reload));
+	watch(paths.srcPath.img, series(imageBuild, reload));
+}
+
+exports.default = series(
+	init,
+	parallel(htmlBuild, cssBuild, jsBuild, imageBuild),
+	browserSync,
+	watcher
+);
